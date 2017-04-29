bd858181d0753105729c513075dbfa9d72b431b0
Aditya <aditya@example.com>
2017-04-29 14:20:57 +0530

    I added one more function multiply to my code

--- calc.py
+++ calc.py
@@ -7,6 +7,11 @@ class Calculator:
 	
 	def sub(self, x, y):
 		return x-y
+
+	def multiply(self, x, y):
+		return x*y
+
 cal = Calculator()
 print(cal.add(2,3))
 print(cal.sub(5,2))
+print(cal.multiply(6,3))
