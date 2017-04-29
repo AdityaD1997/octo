902c701952bf777691d01f33f2448342a7db0e99
Aditya <aditya@example.com>
2017-04-29 14:34:40 +0530

    added division function

--- calc.py
+++ calc.py
@@ -11,7 +11,10 @@ class Calculator:
 	def multiply(self, x, y):
 		return x*y
 
+	def divide(self, x,y):
+		return x/y
 cal = Calculator()
 print(cal.add(2,3))
 print(cal.sub(5,2))
 print(cal.multiply(6,3))
+print(cal.divide(6,3))
